bcd49afb934e0bcb8ce2d0f469b47b7c9f3629a5
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2025-03-23 18:24:15 -0300

    Revert "Use EmptyBuildItem"

--- deployment/src/main/java/io/quarkiverse/custom/deployment/items/CheckpointBuildItem.java
+++
@@ -1,7 +0,0 @@
-package io.quarkiverse.custom.deployment.items;
-
-import io.quarkus.builder.item.EmptyBuildItem;
-
-public final class CheckpointBuildItem extends EmptyBuildItem {
-    // empty data
-}
