fb0df30d61ee2730072aa4a4e62dab79c721b94c
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-03-23 18:55:49 -0300

    Produce and consume EmptyBuildItem

---
+++ deployment/src/main/java/io/quarkiverse/custom/deployment/items/EmptyCustomBuildItem.java
@@ -0,0 +1,7 @@
+package io.quarkiverse.custom.deployment.items;
+
+import io.quarkus.builder.item.EmptyBuildItem;
+
+public final class EmptyCustomBuildItem extends EmptyBuildItem {
+    // empty
+}
