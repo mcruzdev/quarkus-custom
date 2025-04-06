f887fe3979d0de2d3c6b551cad372d58ffc97f56
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2025-04-06 19:33:06 -0300

    Use ApplicationIndexBuildItem (#11)

--- deployment/src/main/java/io/quarkiverse/custom/deployment/MockProcessor.java
+++ deployment/src/main/java/io/quarkiverse/custom/deployment/MockProcessor.java
@@ -1,10 +1,17 @@
 package io.quarkiverse.custom.deployment;
 
+import java.util.List;
+
+import org.jboss.jandex.AnnotationInstance;
+import org.jboss.jandex.DotName;
+import org.jboss.jandex.Index;
+
+import io.quarkiverse.custom.deployment.items.MockBuildItem;
+import io.quarkiverse.custom.runtime.GETMock;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.annotations.Produce;
 import io.quarkus.deployment.builditem.ApplicationIndexBuildItem;
 import io.quarkus.deployment.builditem.LaunchModeBuildItem;
-import io.quarkus.deployment.pkg.builditem.ArtifactResultBuildItem;
 import io.quarkus.runtime.LaunchMode;
 
 public class MockProcessor {
@@ -12,13 +19,24 @@ public class MockProcessor {
     // 1. Executar apenas se for em modo de desenvolvimento
 
     @BuildStep
-    @Produce(ArtifactResultBuildItem.class)
-    void generateMock(LaunchModeBuildItem launchModeBuildItem, ApplicationIndexBuildItem applicationIndexBuildItem) {
+    void generateMock(LaunchModeBuildItem launchModeBuildItem, ApplicationIndexBuildItem applicationIndexBuildItem,
+            BuildProducer<MockBuildItem> mocks) {
         LaunchMode launchMode = launchModeBuildItem.getLaunchMode();
         System.out.println("Launch mode is " + launchMode.name());
         if (!launchModeBuildItem.getLaunchMode().isDevOrTest()) {
             return;
         }
 
+        Index index = applicationIndexBuildItem.getIndex();
+
+        List<AnnotationInstance> annotations = index.getAnnotations(DotName.createSimple(GETMock.class));
+        for (AnnotationInstance annotation : annotations) {
+            String path = annotation.value("path").asString();
+            String responseBody = annotation.value("responseBody").asString();
+            int status = annotation.value("status").asInt();
+
+            // validações
+            mocks.produce(new MockBuildItem(path, responseBody, status));
+        }
     }
 }
